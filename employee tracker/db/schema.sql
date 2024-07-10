c22e6785457a3157ac8c01137ec2dc29dee65422
CharlesHut <148402227+CharlesHut@users.noreply.github.com>
2024-07-10 14:45:20 -0700

    adding foreign keys and references

--- employee tracker/db/schema.sql
+++ employee tracker/db/schema.sql
@@ -8,22 +8,18 @@ create table department(
 
 create table role(
    id SERIAL PRIMARY KEY,
-
-title VARCHAR(30) UNIQUE NOT NULL,
-
-salary DECIMAL NOT NULL, 
-
-department_id INTEGER NOT NULL
+   title VARCHAR(30) UNIQUE NOT NULL,
+   salary DECIMAL NOT NULL,
+   department_id INTEGER NOT NULL,
+   FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 create table employee(
-id SERIAL PRIMARY KEY,
-
-first_name VARCHAR(30) NOT NULL,
-
-last_name VARCHAR(30) NOT NULL,
-
-role_id INTEGER NOT NULL, 
-
-manager_id INTEGER 
+   id SERIAL PRIMARY KEY,
+   first_name VARCHAR(30) NOT NULL,
+   last_name VARCHAR(30) NOT NULL,
+   role_id INTEGER NOT NULL,
+   manager_id INTEGER,
+   FOREIGN KEY (role_id) REFERENCES role(id),
+   FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
